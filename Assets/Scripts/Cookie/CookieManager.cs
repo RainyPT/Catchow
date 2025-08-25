00f62d8bcb612f9043196ece3faabf814fac9583
Calinas <calinaspentas@gmail.com>
2025-08-25 14:59:16 +0100

    add: Adding Cookie
    Creating the cookie prefab, rotation and destruction when colliding with player

---
+++ Assets/Scripts/Cookie/CookieManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class CookieManager : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 50f; // Rotation Speed (editable in inspector)
+    private float currentY;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // Rotation Animation
+        currentY += rotationSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(
+            transform.rotation.eulerAngles.x,
+            currentY,
+            transform.rotation.eulerAngles.z
+        );
+    }
+    
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }    
+    }
+}
